cd81eed3ee430a908747e570ea4ff800a0a19890
Ryong1998 <50094903+Ryong1998@users.noreply.github.com>
2021-09-10 17:51:13 +0900

    first commint

---
+++ src/practice/quiz_2.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class quiz_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+		
+		double radius = 10.0;
+		double circleArea = radius*radius*PI;
+		
+		System.out.println(circleArea); //소문자 system.out 으로 하면 안됨
+	}
+
+}
